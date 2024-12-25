515afc7096357c0b58af7fbcb5e773659f5fdfed
DrZ199 <190632761+DrZ199@users.noreply.github.com>
2024-12-26 03:21:37 +0500

    delete

--- app/store/rootSaga.ts
+++
@@ -1,7 +0,0 @@
-import { all } from "redux-saga/effects";
-
-import { messagesSaga } from "../features/messages/messagesSagas";
-
-export default function* rootSaga() {
-  yield all([messagesSaga()]);
-}
